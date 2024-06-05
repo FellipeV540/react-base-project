671d3f575d022bae3be25968a5c4776f14483d20
bruno <brunobittencourtchequer@hotmail.com>
2024-06-05 15:31:50 -0300

    commit 05/06/24

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,34 @@ const Container = styled.article`
     margin-inline: auto;
     gap: 40px;
   }
-
+  th{
+    display:table-cell;
+    font-weight:normal;
+    
+  }
+  tr{
+    background-color:white;
+    padding:5px;
+    border:none;
+    border-radius:10px;
+  }
+.table-area{
+  padding:10px;
+  background-color:#bfbfbf;
+  border-radius:20px;
+  color:black;
+}
+.table-rodape{
+  display:flex;
+  justify-content:space-between;
+}
+.table-area div{
+  background-color:white;
+  padding:5px;
+  border-radius:10px;
+  margin-bottom:5px;
+  font-size:20px;
+}
   .welcome-message {
     text-align: center;
     font-size: 24px;
@@ -34,6 +61,57 @@ const Container = styled.article`
 `;
 
 const Home = () => {
+  const dados = [
+    {
+      icon: 'imagens/icones/icon-1.png',
+      status: 'Pendente',
+      id: '#131',
+    },
+    {
+      icon: 'imagens/icones/icon-2.png',
+      status: 'Concluído',
+      id: '#132',
+    },
+    {
+      icon: 'imagens/icones/icon-3.png',
+      status: 'Pendente',
+      id: '#133',
+    },
+    {
+      icon: 'imagens/icones/icon-4.png',
+      status: 'Pendente',
+      id: '#134',
+    },
+    {
+      icon: 'imagens/icones/icon-2.png',
+      status: 'Concluído',
+      id: '#135',
+    },
+    {
+      icon: 'imagens/icones/icon-1.png',
+      status: 'Pendente',
+      id: '#136',
+    },
+    {
+      icon: 'imagens/icones/icon-1.png',
+      status: 'Em Processo',
+      id: '#137',
+    },
+
+
+  ]
+  const dados_notificacoes = [
+    {
+      nome: 'Cliníca Albert',
+      servico: 'Dosimetria Clínica',
+      id: '#16723',
+    },
+    {
+      nome: 'Cliníca Robert',
+      servico: 'Modelagem Computacional',
+      id: '#123',
+    },
+  ]
   const calculatePendingProcesses = (type) => {
     return exames
       .filter(item => item.type === type)
@@ -51,36 +129,39 @@ const Home = () => {
         </p>
 
         <div className="module-cards-container">
-          <ModuleCard 
-            moduleName="Dosimetria Clínica" 
-            icon={<RiHospitalLine />} 
-            route="/clinica" 
-            processosEmAberto={calculatePendingProcesses("Dosimetria Clinica")} 
-          />
-          <ModuleCard 
-            moduleName="Dosimetria Pré-Clínica" 
-            icon={<RiHospitalLine />} 
-            route="/dosimetriapreclinica" 
-            processosEmAberto={calculatePendingProcesses("Dosimetria Pré-Clinica")} 
-          />
-          <ModuleCard 
-            moduleName="Modelagem Computacional" 
-            icon={<RiComputerLine />} 
-            route="/modelagem" 
-            processosEmAberto={calculatePendingProcesses("Modelagem Computacional")} 
-          />
-          <ModuleCard 
-            moduleName="Radiosinoviortese" 
-            icon={<RiBookOpenLine />} 
-            route="/radiosinoviortese" 
-            processosEmAberto={calculatePendingProcesses("Radiosinoviorte")} 
-          />
-          <ModuleCard 
-            moduleName="Segmentação e Quantificação" 
-            icon={<RiScissors2Line />} 
-            route="/SegmentacaoeQuantificacao" 
-            processosEmAberto={calculatePendingProcesses("Segmentação e Quantificação")} 
-          />
+          <div className='table-area'>
+            {dados_notificacoes.map((item) => (
+              <div>{item.nome} Contratou Serviço de {item.servico} - id {item.id}</div>
+            ))}
+          </div>
+          <div className='table-area'>
+            <table>
+              <tr>
+                <th>Serviços</th>
+                <th>ID</th>
+                <th>Para Calibração</th>
+                <th>Imagem Paciente</th>
+                <th>Enviar Relatório</th>
+                <th>Status</th>
+                <th>Arquivar</th>
+              </tr>
+              {dados.map((item) => (
+                <tr>
+                  <td> <img src={process.env.PUBLIC_URL + item.icon} alt="Imagem não Carregou" /></td>
+                  <td>{item.id}</td>
+                  <td><img src={process.env.PUBLIC_URL + 'imagens/icones/icon-5.png'} alt="Imagem não Carregou" /></td>
+                  <td><img src={process.env.PUBLIC_URL + 'imagens/icones/icon-5.png'} alt="Imagem não Carregou" /></td>
+                  <td><img src={process.env.PUBLIC_URL + 'imagens/icones/icon-6.png'} alt="Imagem não Carregou" /></td>
+                  <td>{item.status}</td>
+                  <td><img src={process.env.PUBLIC_URL + 'imagens/icones/icon-7.png'} alt="Imagem não Carregou" /></td>
+                </tr>
+              ))}
+            </table>
+           <div className='table-rodape'> <h4>Serviços Pendentes 15</h4>
+           <h4>Serviços Concluídos 32</h4>
+           </div>
+          
+          </div>
         </div>
       </Container>
     </Base>
