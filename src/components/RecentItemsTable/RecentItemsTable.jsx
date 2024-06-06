28f3b4b1191b00e5c869eca9fddba94c75fbbe1f
FELLIPE MARTINS VALLADARES <202401000991@alunos.ibmec.edu.br>
2024-06-06 09:49:18 -0300

    layout home e radiosinoviortese reformulados, e altrações recentitems

--- src/components/RecentItemsTable/RecentItemsTable.jsx
+++ src/components/RecentItemsTable/RecentItemsTable.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import {
-  RiHospitalLine,
-  RiBookOpenLine,
-  RiComputerLine,
-  RiScissors2Line,
+  RiDownload2Line,
+  RiFileUploadLine,
+  RiArchive2Line,
   RiArrowRightUpLine,
 } from "react-icons/ri";
 import { IconButton } from "../Buttons/Buttons";
-
 
 const StyledTable = styled.table`
   width: 100%;
@@ -19,7 +17,6 @@ const StyledTable = styled.table`
     border: 4px solid green;
     border-radius: 10px;
     overflow: hidden;
-    /* background-color: lightgray; */
   }
   th,
   td {
@@ -27,11 +24,11 @@ const StyledTable = styled.table`
     outline: none !important;
   }
 
-  tr{
+  tr {
     border-radius: 0px !important;
 
-    &:hover{
-       outline: 2px solid lightblue;
+    &:hover {
+      outline: 2px solid lightblue;
     }
   }
   th {
@@ -39,7 +36,6 @@ const StyledTable = styled.table`
     font-weight: 600 !important;
     background-color: var(--leve);
     color: white;
-    
   }
   tr:nth-child(even) {
     background-color: #f2f2f2; /* Cor de fundo para linhas pares */
@@ -49,7 +45,7 @@ const StyledTable = styled.table`
     background-color: #ffffff; /* Cor de fundo para linhas impares */
   }
 
-  .dots{
+  .dots {
     display: flex;
     align-items: center;
     justify-content: center;
@@ -57,7 +53,7 @@ const StyledTable = styled.table`
     font-weight: 800;
     color: var(--leve);
   }
-  .info{
+  .info {
     color: gray;
     padding: 10px;
     font-size: 14px;
@@ -65,84 +61,67 @@ const StyledTable = styled.table`
 `;
 
 const RecentItemsTable = (props) => {
-  const modules ={
-    "Dosimetria Clinica": {
-      icone: <RiHospitalLine />,
-      rota: "/clinica"
-    },
-    "Dosimetria Pré-Clinica": {
-      icone: <RiHospitalLine />,
-      rota: "/dosimetriapreclinica"
-    },
-    "Modelagem Computacional": {
-      icone: <RiComputerLine />,
-      rota: "/modelagem"
-    },
-    "Radiosinoviortese": {
-      icone: <RiBookOpenLine />,
-      rota: "/radiosinoviortese"
-    },
-    "Segmentação e Quantificação": {
-      icone: <RiScissors2Line />,
-      rota: "/SegmentacaoeQuantificacao"
-    }
-  }
-
-  const getMostRecentItems = () => {
-    const recentItems = [];
-
-    props.exames.forEach((item) => {
-      const { type, itens } = item;
-      const mostRecentItem = itens.reduce((prev, current) => {
-        return new Date(prev.data) > new Date(current.data) ? prev : current;
-      });
-
-      recentItems.push({ ...mostRecentItem, type: type });
-    });
-
-    return recentItems.filter((item) => item.status === "Pendente").slice(0, 5);
-  };
-
-  const recentItems = getMostRecentItems();
-
   return (
-  
-      <StyledTable>
-        <div className="table-container">
-          <table>
-            <thead>
-              <tr>
-                <th>Type</th>
-                <th>Description</th>
-                <th>Status</th>
-                <th>Date</th>
-                <th>Document</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.values(recentItems).map((item, index) => (
-                <tr key={index}>
+    <StyledTable>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Serviço</th>
+              <th>Nome da Clínica</th>
+              <th>ID</th>
+              <th>Data</th>
+              <th>Imagem Paciente</th>
+              <th>Imagem Calibração</th>
+              <th>Status</th>
+              <th>Enviar Relatório</th>
+              <th>Acessar</th>
+              <th>Arquivar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.exames.map((item, index) => (
+              item.itens.map((it, idx) => (
+                <tr key={`${index}-${idx}`}>
                   <td>{item.type}</td>
-                  <td>{item.description}</td>
-                  <td>{item.status}</td>
-                  <td>{item.data}</td>
-                  <td>{item.doc}</td>
+                  <td>{it.nomeClinica}</td>
+                  <td>{it.id}</td>
+                  <td>{it.data}</td>
+                  <td>
+                    <IconButton>
+                      <RiDownload2Line />
+                    </IconButton>
+                  </td>
+                  <td>
+                    <IconButton>
+                      <RiDownload2Line />
+                    </IconButton>
+                  </td>
+                  <td>{it.status}</td>
+                  <td>
+                    <IconButton>
+                      <RiFileUploadLine />
+                    </IconButton>
+                  </td>
                   <td>
                     <IconButton>
                       <RiArrowRightUpLine />
                     </IconButton>
                   </td>
+                  <td>
+                    <IconButton>
+                      <RiArchive2Line />
+                    </IconButton>
+                  </td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-            <p className="dots">
-                . . .
-            </p>
-            <p className="info">para ver mais, acesse algum modulo acima</p>
-        </div>
-      </StyledTable>
+              ))
+            ))}
+          </tbody>
+        </table>
+        <p className="dots">. . .</p>
+        <p className="info">para ver mais, acesse algum modulo acima</p>
+      </div>
+    </StyledTable>
   );
 };
 
--- src/pages/Radiosinoviortese.jsx
+++ src/pages/Radiosinoviortese.jsx
@@ -1,127 +1,62 @@
 import React, { useState } from 'react';
-import Base from './Base';
+import {
+    RiHospitalLine,
+    RiBookOpenLine,
+    RiComputerLine,
+    RiScissors2Line,
+} from "react-icons/ri";
+import Base from "./Base";
+import styled from "styled-components";
+import ModuleCard from "../components/ModuleCard/ModuleCard";
+import exames from "../data/exames.json";
+import Modules from "../components/Modules/Modules";
+import RecentItemsTable from "../components/RecentItemsTable/RecentItemsTable";
+import { IconButton } from "../components/Buttons/Buttons";
+
+const Container = styled.article`
+  .content {
+    padding-inline: 200px;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+  }
+
+  .module-cards-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    max-width: 1300px;
+    margin-inline: auto;
+    gap: 30px;
+  }
+
+  .message {
+    text-align: center;
+    font-size: 20px;
+    color: black;
+    margin: 0px;
+  }
+`;
 
 const Radiosinoviortese = () => {
-    const [filter, setFilter] = useState('Todos');
-    const [searchTerm, setSearchTerm] = useState('');
-
-    const dados = [
-        {
-            clinica: 'Clínica Andre',
-            status: 'Pendente',
-            itens: [
-                'Imagem do Cliente :20240-04-19-19:39:54.jpeg',
-                'Imagem para Calibrar: 20240-04-19-19:39:54.jpeg',
-            ],
-            botoes: [
-                'Enviar'
-            ],
-            processo: '0010',
-
-        },
-        {
-            clinica: 'Clínica Lucas',
-            status: 'Concluído',
-            itens: [
-                'Imagem do Cliente :20240-04-19-19:39:54.jpeg',
-                'Imagem para Calibrar: 20240-04-19-19:39:54.jpeg',
-            ],
-            botoes: [
-                'Enviar',
-                'Arquivar'
-            ],
-            processo: '0011',
-        },
-        {
-            clinica: 'Clínica Giovana',
-            status: 'Concluído',
-            itens: [
-                'Imagem do Cliente :20240-04-19-19:39:54.jpeg',
-                'Imagem para Calibrar: 20240-04-19-19:39:54.jpeg',
-            ],
-            botoes: [
-                'Enviar',
-                'Arquivar'
-            ],
-            processo: '0012'
-        },
-    ];
-
-    const handleFilterChange = (newFilter) => {
-        setFilter(newFilter);
-    };
-
-    const handleSearchChange = (event) => {
-        setSearchTerm(event.target.value);
-    };
-
-    const filteredDados = dados.filter((item) => {
-        const matchesFilter = filter === 'Todos' || item.status === filter;
-        const matchesSearchTerm = item.clinica.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            item.processo.includes(searchTerm) ||
-            item.itens.some(it => it.toLowerCase().includes(searchTerm.toLowerCase()));
-        return matchesFilter && matchesSearchTerm;
-    });
+    // Filtrar exames do tipo "Radiosinoviortese"
+    const radiosinoviorteseExames = exames.filter(exame => exame.type === "Radiosinoviortese");
 
     return (
         <Base>
-            <h1>Radiosinoviortese</h1>
-            <div className="dosimetria-sub">
-                <h3>Clientes</h3>
-                <h3>{filteredDados.length} {filter}</h3>
-            </div>
-            <div className="search-bar">
-            <input
-                    type="text"
-                    placeholder="Pesquisar..."
-                    value={searchTerm}
-                    onChange={handleSearchChange}
-                />
-            </div>
-            <div className="filter-buttons">
-                <button onClick={() => handleFilterChange('Todos')}>Todos</button>
-                <button onClick={() => handleFilterChange('Pendente')}>Pendente</button>
-                <button onClick={() => handleFilterChange('Concluído')}>Concluído</button>
-            </div>
-            {filteredDados.map((tipo) => {
-                return (
-                    <div className="dosimetria-area" key={tipo.processo}>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>
-                                        <span>
-                                            {tipo.clinica}
-                                        </span>
-                                        <span className="processo">
-                                            Processo: {tipo.processo}
-                                        </span>
-                                        <span className={`item-status ${tipo.status === 'Pendente' ? 'status-pendente' : 'status-concluido'}`}>
-                                            Status: {tipo.status}
-                                        </span>
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {tipo.itens.map((item, index) => {
-                                    return (
-                                        <tr key={index}>
-                                            <td className="item-img"><span>{item}</span><a href="">Download</a> </td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                        <div className="btn-area">
-                            {tipo.botoes.map((item, index) => {
-                                return (
-                                    <button key={index}>{item}</button>
-                                );
-                            })}
-                        </div>
-                    </div>
-                );
-            })}
+            <Container>
+                <div className="content">
+                    <h2 className="message">PROCESSOS DE RADIOSINOVIORTESE</h2>
+                    <p className="message">
+                        Aqui você pode acessar todos os procedimentos de radiosinoviortese.
+                    </p>
+                    <p className="message">
+                        Todos os Procedimentos
+                    </p>
+                    <RecentItemsTable exames={radiosinoviorteseExames} />
+                </div>
+            </Container>
         </Base>
     );
 };
